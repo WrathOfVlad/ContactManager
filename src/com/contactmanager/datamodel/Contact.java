3a801ffc291d2b907fcc0fe8b74298441d7a688e
WrathOfVlad <tizidac03@gmail.com>
2021-07-01 11:55:26 +0100

    Fixed add new contact not working

--- src/com/contactmanager/datamodel/Contact.java
+++ src/com/contactmanager/datamodel/Contact.java
@@ -25,13 +25,24 @@ public class Contact {
 	private Map<String, DataItemHandler> dataMap = new LinkedHashMap<String, DataItemHandler>();
 	
 	public Contact(Map<String, String> rowData){
-		Map<String, Map<String, Object>> metaData = ConfigFileData.getInstance().getItemMetaData();
 		if(rowData == null) {
 			setId("");
 			return;
 		}
 		setId(rowData.get(ID_FIELD));
+		createContact();
 		
+		Map<String, DataItemHandler> tempDataMap = new HashMap<>(dataMap);
+		for (String textField : rowData.keySet()) {
+			if(textField.equals(ID_FIELD)) {continue;}
+			tempDataMap.get(textField).setValue(rowData.get(textField));
+		}
+		dataMap = tempDataMap;
+		setFullName();
+		
+	}
+	private void createContact() {
+		Map<String, Map<String, Object>> metaData = ConfigFileData.getInstance().getItemMetaData();
 		ItemFactory itemFactory = new ItemFactory();
 		for (String dataId : metaData.keySet()) {
 			
@@ -40,15 +51,11 @@ public class Contact {
 				dataItem = itemFactory.getDataItem(dataId, metaData.get(dataId));
 			} catch (Exception e) {
 				e.printStackTrace();
-			}
-			dataItem.setValue(rowData.get(dataId));
-			
+			}			
 			addToMap(dataId, dataItem);
 		}
-
-		setFullName();
-		
 	}
+	
 	private void addToMap(String dataId,DataItemHandler dataItem) {
 		dataMap.put(dataId, dataItem);
 	}
@@ -71,11 +78,13 @@ public class Contact {
 	
 
 	public Boolean saveFromContactDetailView(Map<String, JTextField> textFieldMap) {
+		if(dataMap.isEmpty()) {
+			createContact();
+		}
 		Map<String, DataItemHandler> tempDataMap = new HashMap<>(dataMap);
 		List<Boolean> isValidList = new ArrayList<>();
 		for (String textField : textFieldMap.keySet()) {
 			isValidList.add(tempDataMap.get(textField).setValue(textFieldMap.get(textField).getText()));
-		
 		}
 		if(isValidList.contains(false)) {return false;}
 		
--- src/com/contactmanager/vew/MainFrame.java
+++ src/com/contactmanager/vew/MainFrame.java
@@ -31,7 +31,7 @@ public class MainFrame extends JFrame {
 	public static final String CONTACT_LOG = "ContactLog";
 	public static final String SETTINGS = "Settings";
 	public static final String EMPTY = "empty";
-	public static final String VERSION = "2.4.2";
+	public static final String VERSION = "2.4.3";
 	
 	public boolean isContactListViewerUpToDate = true;
 	
