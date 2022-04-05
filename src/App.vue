55c8a5bb0a399c22e6835e6bf2c2d1045fdf02c0
WrathOfVlad <tizidac03@gmail.com>
2022-04-06 00:23:24 +0100

    First Commit Created a new Vue project and began experimenting with vue. Created a ToDo template, allowing users to easily add and remove items to the todo, and allowing them to easily add new independent todo lists with custom names

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { ref } from 'vue'
+import ToDo from './views/ToDo.vue';
+
+const msg = ref('Hello World!')
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+  <input v-model="msg">
+  <ToDo title="First Todo"></ToDo>
+  <ToDo></ToDo>
+</template>
---
+++ src/views/ToDo.vue
@@ -0,0 +1,51 @@
+<template>
+    <h1>{{ title }}</h1>
+    <ul>
+        <li :style="{color:isRemove ? 'red' : 'black' , cursor: isRemove ? 'pointer':'default'}" @click="elementClicked(index)" v-for="(element,index) in todoElements" v-bind:key="index"> 
+            {{ element }}
+        </li>
+    </ul>
+    <input v-model="newItem" />
+    <button @click="addElement">Add</button>
+
+    <button @click="isRemove = !isRemove">Toggle Remove</button>
+</template>
+
+<script>
+    import { ref } from 'vue'
+    export default {
+        
+        props:{
+          title: { 
+              type: String,
+              default: "ToDo"
+            }
+        },
+
+        setup() {
+            const todoElements = ref(['John',"bananna"]);
+            const newItem = ref("");
+            const isRemove = ref(false);
+
+            const elementClicked = (key) => {
+                if(isRemove.value){
+                    console.log(key);
+                    todoElements.value.splice(key,1);
+                }
+            }
+
+            const addElement = () => {
+                todoElements.value.push(newItem.value);
+                newItem.value = "";
+            }
+
+            return{
+                todoElements,
+                newItem,
+                isRemove,
+                addElement,
+                elementClicked
+            }
+        }
+    }
+</script>
